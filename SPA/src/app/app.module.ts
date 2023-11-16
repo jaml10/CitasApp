ae1f5814891ab70cc55520ff1281bd3d88378fef
jaml10 <jesus.alomeli@gmail.com>
2023-11-16 08:27:49 -0600

    Seccion09 - Final commit - Added ng-gallery

--- SPA/src/app/app.module.ts
+++ SPA/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -29,7 +30,6 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
     HomeComponent,
     RegisterComponent,
     MemberListComponent,
-    MemberDetailComponent,
     ListsComponent,
     MessagesComponent,
     TestErrorComponent,
--- SPA/src/app/members/member-detail/member-detail.component.ts
+++ SPA/src/app/members/member-detail/member-detail.component.ts
@@ -1,27 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { IMember } from 'src/app/_models/imember';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
+  standalone: true,
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  styleUrls: ['./member-detail.component.css'],
+  imports: [CommonModule, TabsModule, GalleryModule]
 })
 export class MemberDetailComponent implements OnInit {
   member: IMember | undefined;
 
-  constructor(private memberService: MembersService, private route: ActivatedRoute) {}
+  constructor(private membersService: MembersService, private route: ActivatedRoute) {}
+  images: GalleryItem[] = [];
 
   ngOnInit(): void {
-  this.loadMember();
+    this.loadMember();
   }
-
-  loadMember(){
+  loadMember() {
     const username = this.route.snapshot.paramMap.get("username");
-    if(!username) return;
-    this.memberService.getMember(username).subscribe({
-      next: member => this.member = member
+    if (!username) return;
+    this.membersService.getMember(username).subscribe({
+      next: member => {
+        this.member = member,
+          this.getImages()
+      }
     });
   }
+
+  getImages() {
+    if (!this.member) return;
+    for (const photo of this.member?.photos) {
+      this.images.push(new ImageItem({ src: photo.url, thumb: photo.url }));
+      this.images.push(new ImageItem({ src: photo.url, thumb: photo.url }));
+    }
+  }
+
 }
