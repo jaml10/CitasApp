140e075bcb3fd465dac3b8adceaed56322523864
jaml10 <jesus.alomeli@gmail.com>
2024-02-20 10:28:02 -0600

    20230220_linux

---
+++ API/Interfaces/IPhotoService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cloudinary.
+
+namespace API.Interfaces
+{
+    public class IPhotoService
+    {
+        Task<ImageUploadResult>
+    }
+}
