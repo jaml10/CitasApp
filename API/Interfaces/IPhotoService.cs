780d2304cb1292877a40b283e8df169d32b304f6
jaml10 <jesus.alomeli@gmail.com>
2024-02-20 16:33:20 -0600

    20230220_Virtual

--- API/Interfaces/IPhotoService.cs
+++ API/Interfaces/IPhotoService.cs
@@ -1,13 +1,15 @@
 using System;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Cloudinary.
+using Cloudinary.Actions;
 
 namespace API.Interfaces
 {
     public class IPhotoService
     {
-        Task<ImageUploadResult>
+        Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+        Task<DeletionResult> DeletePhotoAsync(string publicId); 
     }
 }
---
+++ API/Services/PhotoService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.interface;
+
+namespace API.Services
+{
+    public class PhotoService : IPhotoService
+    {
+        throw new NotImplementedException();
+    }
+    public Task<DeletionResult> DeletePhotOaSYNC(string PUBLICid)
+    {
+        throw new NotImplemenedException(),
+    }
+}
